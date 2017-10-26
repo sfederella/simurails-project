cd6afd2cbb7a619d72ef4fc5eab06e24bdf26aa2
belenlup <belen.lupani@hotmail.com>
2017-10-26 20:22:23 -0300

    Retoque en reporte para que no pinche

--- Desarrollo/SimuRails/Report/ReportHelperValorIdaVuelta.cs
+++ Desarrollo/SimuRails/Report/ReportHelperValorIdaVuelta.cs
@@ -9,8 +9,31 @@ namespace SimuRails.Report
     class ReportHelperValorIdaVuelta
     {
         public string Nombre { get; set; }
-        public double ValorIda { get; set; }
-        public double ValorVuelta { get; set; }
+
+        private double pValorIda;
+        public double ValorIda
+        {
+            get
+            {
+                return pValorIda;
+            }
+            set
+            {
+                if (value > 0) { pValorIda = value; } else { pValorIda = 0; }
+            }
+        }
+        private double pValorVuelta;
+        public double ValorVuelta
+        {
+            get
+            {
+                return pValorVuelta;
+            }
+            set
+            {
+                if (value > 0) { pValorVuelta = value; } else { pValorVuelta = 0; }
+            }
+        }
         public ReportHelperValorIdaVuelta(string nombre, double valorIda, double valorVuelta)
         {
             Nombre = nombre;
