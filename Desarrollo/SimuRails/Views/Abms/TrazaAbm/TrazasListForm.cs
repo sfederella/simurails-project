ee79a2cc1c2c2ef5968552bcde0d7c6221d6b89c
Ramiro Vazquez <ramirov93@gmail.com>
2017-11-12 16:40:48 -0300

    saco comentario y metodo de listar traza innecesario

--- Desarrollo/SimuRails/Views/Abms/TrazaAbm/TrazasListForm.cs
+++ Desarrollo/SimuRails/Views/Abms/TrazaAbm/TrazasListForm.cs
@@ -145,12 +145,6 @@ namespace SimuRails.Views.Abms
                 ImportarTraza(trazaRecuperada);
             }
             
-            //TODO: Recargar los listados de los otros ABM de alguna manera luego de importar.
-            using (var repositorio = new Repositorio())
-            {
-                dibujarRenglones(repositorio);
-            }
-
             Application.Restart();
         }
 
